cec1cd55e65a0133db745b35ea28fe8e9cd40823
Alex Nascimento <ancmto@gmail.com>
2025-12-29 21:00:56 +0200

    feat: Integrate Tailwind CSS typography and animate plugins, refactor global styles with `@theme` and `@layer` directives, and remove `postcss.config.js`.

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {}, // autoprefixer é opcional no v4, mas não faz mal manter se estiver instalado e configurado
-  },
-}
